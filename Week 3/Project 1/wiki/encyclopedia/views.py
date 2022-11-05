a2f56b8be63483bc0c9bc3ad6242ae423cd2da5d
Ari Jigarjian <ajigarjian@gmail.com>
2022-11-05 19:02:05 -0400

    first part of project 1

--- Week 3/Project 1/wiki/encyclopedia/views.py
+++ Week 3/Project 1/wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -7,4 +8,21 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def page(request, page):
+    if page.lower() == "css" or page.lower() == "html":
+            page = page.upper()
+    else:
+        page = page.capitalize()
+
+    file_name = util.get_entry(page)
+
+    if file_name == None:
+        return render(request, "encyclopedia/error.html")
+    
+    else:
+        return render(request, "encyclopedia/page.html", {
+            "page": page,
+            "content": markdown2.markdown(file_name)
+        })
 
